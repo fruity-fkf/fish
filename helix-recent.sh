c90d0e52fbb771198a4307cff91ac3c84d2e6abc
fruity-fkf <fkf.6106@gmail.com>
2024-10-16 23:34:05 +0200

    cleanup

--- helix-recent.sh
+++ helix-recent.sh
@@ -1,24 +1,14 @@
 #!/usr/bin/env bash
 SED="sed"
 HX=hx
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    SED="gsed"
-fi
 RECENT=~/.cache/recent_hx
 FILE=${@:1}
-
 if [ ! "$FILE" ]; then
     FILE=$(tac $RECENT | fzf)
 fi
-
 if [ "$FILE" ]; then
-    # Resolve full path
     FILE=$(printf "%s" "$(cd "$(dirname -- "$FILE")" >/dev/null; pwd -P)/$(basename -- "$FILE")")
-    
-    # Remove the file from the RECENT list if it already exists
     $SED -i "\#$FILE#d" $RECENT
-    
-    # Append the new file to the RECENT list
     echo "$FILE" >> $RECENT
 fi
 
